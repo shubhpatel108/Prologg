c591aae14313a6837f0583d239919e9738701b0f
Shubham Patel <shubhpatel108@gmail.com>
2015-03-06 23:35:02 +0530

    Add more fields to register and edit User profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :update_sanitized_params_signup, if: :devise_controller?
+  before_filter :update_sanitized_params_edit, if: :devise_controller?
+
+	def update_sanitized_params_signup
+		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :full_name, :username, :gender)}
+	end
+
+	def update_sanitized_params_edit
+		devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password, :full_name, :username, :gender)}
+	end
 end
