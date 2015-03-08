c08c800e248964627ccfeacdba94a902f45c2729
Shubham Patel <shubhpatel108@gmail.com>
2015-03-09 02:08:25 +0530

    Fetch tags, languages and change in ratings data and store as JSON for CodeforcesProfile

--- app/controllers/codeforces_profile_controller.rb
+++ app/controllers/codeforces_profile_controller.rb
@@ -6,24 +6,67 @@ class CodeforcesProfileController < ApplicationController
 
 	def create
 		handle = params[:handle]
-		response = CodeforcesProfile.connect(handle)
-		status = response[0]
-		resp = response[1]["result"][0]
-		if status == "OK"
-			CodeforcesProfile.create(
+
+		response_info = CodeforcesProfile.connect(handle)
+		status_info = response_info[0]
+		resp_info = response_info[1]["result"][0]
+
+		response_subs = CodeforcesProfile.get_submissions(handle)
+		status_subs = response_subs[0]
+		resp_subs = response_subs[1]["result"]
+
+		response_rates = CodeforcesProfile.get_ratings(handle)
+		status_rates = response_rates[0]
+		resp_rates = response_rates[1]["result"]
+
+		if status_info == "OK" and status_subs == "OK" and status_rates == "OK"
+			@codeforces_profile = CodeforcesProfile.new(
 				user_id: current_user.id,
-				handle: resp["handle"],
-				contribution: resp["contribution"],
-				rank: resp["rank"],
-				max_rank: resp["maxRank"],
-				rating: resp["rating"],
-				max_rating: resp["maxRating"],
-				last_online: DateTime.strptime(resp["lastOnlineTimeSeconds"].to_s, '%s')
+				handle: handle,
+				contribution: resp_info["contribution"],
+				rank: resp_info["rank"],
+				max_rank: resp_info["maxRank"],
+				rating: resp_info["rating"],
+				max_rating: resp_info["maxRating"],
+				last_online: DateTime.strptime(resp_info["lastOnlineTimeSeconds"].to_s, '%s')
 				)
+
+			solved_probs = 0		#total number of problems solved
+			langs = {}				#hash of hashes - languages => no of problems solved
+			tags_count = {}			#hash of hashes - tags => total no. of problems in which it is found.
+			resp_subs.select do |h|
+				if h["verdict"]=="OK"
+					if langs.key?(h["programmingLanguage"])
+						langs.merge!(h["programmingLanguage"] => langs[h["programmingLanguage"].to_s] + 1)
+					else
+						langs.merge!(h["programmingLanguage"] => 1)
+					end
+					tags = h["problem"]["tags"]
+					tags.each do |tag|
+						if tags_count.key?(tag)
+							tags_count.merge!(tag => tags_count[tag] + 1)
+						else
+							tags_count.merge!(tag => 1)
+						end
+					end
+					solved_probs = solved_probs + 1
+				end
+			end
+
+			resp_rates.each do |h|
+				h.delete("contestId")
+				h.delete("contestName")
+				h.delete("rank")
+			end
+			resp_rates.slice!(resp_rates.length-4..resp_rates.length)
+
+			@codeforces_profile.metadata = { solved_probs: solved_probs, languages: langs, tags: tags_count, ratings: resp_rates }
+			@codeforces_profile.save!
+
 			flash[:notice] = "You have successfully integrated Codeforces in your profile."
 			redirect_to profile_path(username: current_user.username)
 		else
-			flash[:alert] = "It seems your credentials are not authentic."
+			flash[:alert] = "It seems your credentials are not authentic or something went wrong."
 			redirect_to :back
 		end
 	end
--- app/models/codeforces_profile.rb
+++ app/models/codeforces_profile.rb
@@ -4,14 +4,26 @@ class CodeforcesProfile < ActiveRecord::Base
 	def self.get(url)
 		begin
 			resp = RestClient.get(url)
-			JSON.parse(resp)
+			return JSON.parse(resp)
 		rescue
-			return [{"status" => "bad_request"}, {}]
+			return {"status" => "bad_request"}
 		end
 	end
 
 	def self.connect(handle)
 		url = "http://codeforces.com/api/user.info?handles=#{handle}"
+		resp = self.get(url)
+		return [resp["status"], resp]
+	end
+
+	def self.get_submissions(handle)
+		url = "http://codeforces.com/api/user.status?handle=#{handle}&from=1&count=1000"
+		resp = self.get(url)
+		return [resp["status"], resp]
+	end
+
+	def self.get_ratings(handle)
+		url = "http://codeforces.com/api/user.rating?handle=#{handle}"
 		resp = self.get(url)
 		return [resp["status"], resp]
 	end
