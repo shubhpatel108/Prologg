0251839165763052b58e3c22fbda3b0551b4e60b
Prashant Vithani <prashantvithani@gmail.com>
2015-03-09 12:31:33 +0530

    Add session controller to change default route to Profile page after login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   root to: 'profiles#index'
 
   get '/:username', to: 'profiles#show', as: :profile
