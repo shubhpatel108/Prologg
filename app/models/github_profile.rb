70d591344e092f72cd0926e901bb553d03abac49
Shubham Patel <shubhpatel108@gmail.com>
2015-03-12 17:44:36 +0530

    Add GithubProfile model and relate it with User

---
+++ app/models/github_profile.rb
@@ -0,0 +1,3 @@
+class GithubProfile < ActiveRecord::Base
+	belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :omniauthable, :omniauth_providers => [:github]
 
   has_one :gplus_link, dependent: :destroy
   has_one :facebook_link, dependent: :destroy
@@ -12,4 +13,16 @@ class User < ActiveRecord::Base
   has_one :quora_link, dependent: :destroy
 
   has_one :codeforces_profile, dependent: :destroy
+  has_one :github_profile, dependent: :destroy
+
+  def self.from_omniauth(auth, current_user)
+    case auth[:provider]
+    when "github"
+      authorization = GithubProfile.where(:user_id => current_user.id).first
+      if authorization.nil?
+        authorization = GithubProfile.create(:username => auth[:info][:nickname], :user_id => current_user.id)
+      end
+      authorization
+    end
+  end
 end
