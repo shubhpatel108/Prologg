68be4f6aec0b7929f8ccacf1669367db529d1d69
Shubham Patel <shubhpatel108@gmail.com>
2015-03-30 19:17:00 +0530

    Modularize Github Controller code and move important functions to Model

--- app/controllers/github_profile_controller.rb
+++ app/controllers/github_profile_controller.rb
@@ -5,39 +5,24 @@ class GithubProfileController < ApplicationController
 		github = Github.new
 		username = params[:github_username] #here username refers to that of github
 		metadata = {}
+		@github_profile = GithubProfile.where(:user_id => current_user.id).first
 
-		repos = (github.repos.list user: username).body
-		repos.each do |repo|
-			repo.select! {|k,v| ["name", "fork", "stargazers_count", "watchers_count"].include?(k)}
-			contribution = (github.repos.stats.contributors user: username, repo: repo.name).body
-			contribution.select! {|c| c.author.login==username}
-			repo.merge!(:owner_commits => if contribution.empty? then 0 else contribution.map(&:total).sum end)
-			repo.merge!(:languages => (github.repos.languages username, repo.name).body)
-		end
+		repos = @github_profile.repos
 		metadata.merge!(:repos => repos)
 
-		activities = (github.activity.events.performed username).body
-		user_activities = {}
-		user_activities.merge!(:issues_created => activities.map(&:type).select {|e| e=="IssuesEvent"}.count)
-		user_activities.merge!(:total_prs => activities.map(&:type).select {|e| e=="PullRequestEvent"}.count)
-		user_activities.merge!(:merged_prs => activities.select {|e| e.type=="PullRequestEvent" && e.payload.pull_request.merged?}.count)
-		metadata.merge!(:activities => user_activities)
+		activities = @github_profile.activities
+		metadata.merge!(:activities => activities)
 
-		activities_rec = (github.activity.events.received user: username).body
-		metadata.merge!(:interested_people => activities_rec.map(&:actor).uniq.count)
+		activities_received = @github_profile.activities_received
+		metadata.merge!(:interested_people => activities_received)
 
-		metadata.merge!(:followers_count => (github.users.followers.list username).body.count )
+		metadata.merge!(:followers_count => @github_profile.followers_count )
 
-		orgs = (github.orgs.list user: username).body
-		orgs.each do |org|
-			org.select! {|k,v| ["login"].include?(k)}
-			org.merge!(:members_count => (github.orgs.members.list org.login).body.count )
-		end
-
+		orgs = @github_profile.orgs
 		metadata.merge!(:orgs => orgs)
 
-		@github_profile = GithubProfile.where(:user_id => current_user.id).first
 		@github_profile.data = metadata
+		@github_profile.data_will_change!
 		@github_profile.save!
 
 		flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
--- app/models/github_profile.rb
+++ app/models/github_profile.rb
@@ -1,3 +1,47 @@
 class GithubProfile < ActiveRecord::Base
 	belongs_to :user
+
+	def repos
+		github = Github.new
+		repos = (github.repos.list user: self.username).body
+		repos.each do |repo|
+			repo.select! {|k,v| ["name", "fork", "stargazers_count", "watchers_count"].include?(k)}
+			contribution = (github.repos.stats.contributors user: username, repo: repo.name).body
+			contribution.select! {|c| c.author.login==username}
+			repo.merge!(:owner_commits => if contribution.empty? then 0 else contribution.map(&:total).sum end)
+			repo.merge!(:languages => (github.repos.languages username, repo.name).body)
+		end
+		repos
+	end
+
+	def activities
+		github = Github.new
+		activities = (github.activity.events.performed self.username).body
+		user_activities = {}
+		user_activities.merge!(:issues_created => activities.map(&:type).select {|e| e=="IssuesEvent"}.count)
+		user_activities.merge!(:total_prs => activities.map(&:type).select {|e| e=="PullRequestEvent"}.count)
+		user_activities.merge!(:merged_prs => activities.select {|e| e.type=="PullRequestEvent" && e.payload.pull_request.merged?}.count)
+		user_activities
+	end
+
+	def activities_received
+		github = Github.new
+		activities_rec = (github.activity.events.received user: self.username).body
+		activities_rec = activities_rec.map(&:actor).uniq.count
+		activities_rec
+	end
+
+	def followers_count
+		github = Github.new
+		(github.users.followers.list self.username).body.count
+	end
+
+	def orgs
+		github = Github.new
+		orgs = (github.orgs.list user: self.username).body
+		orgs.each do |org|
+			org.select! {|k,v| ["login"].include?(k)}
+			org.merge!(:members_count => (github.orgs.members.list org.login).body.count )
+		end
+	end
 end
