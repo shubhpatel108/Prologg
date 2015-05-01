af66571b9ce9f836d47f0dc4ebd04714d45f26ae
Prashant Vithani <prashantvithani@gmail.com>
2015-05-01 16:37:15 +0530

    Add default urls options to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer.default_url_options = { host: 'prologg.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "webmail.daiict.ac.in",
